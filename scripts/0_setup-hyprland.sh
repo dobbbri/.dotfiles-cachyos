793fddb625c44213452b201c9ba27af4c14e353d
Sergio Dobri <sergiodobri@gmail.com>
2026-03-23 11:13:01 -0300

    update...

--- scripts/0_setup-hyprland.sh
+++ scripts/0_setup-hyprland.sh
@@ -9,25 +9,25 @@ sudo paru -S --needed --noconfirm waybar bitwarden tumbler firefox swww thunar \
     fuzzel nwg-look qt6-wayland github-desktop nordic-theme papirus-icon-theme \
     cmake go zig uv typst brightnessctl ttf-jetbrains-mono-nerd imv mpv \
     gimp viu wl-clipboard clang docker docker-composer gvfs gvfs-mtp libmtp python3 go rust cargo \
-  ripgrep lua51 luarocks wget stow python-pip python-pynvim wl-clipboard htop kitty thunar-archive-plugin
+  ripgrep lua51 luarocks wget stow python-pip python-pynvim wl-clipboard htop kitty thunar-archive-plugin ranger
 
 # create a new symlink
 sudo ln -s /usr/bin/python3 /usr/bin/python
-
-echo "Adding user to input group..."
-sudo usermod -aG input "$USER"
 
 echo "Setting up SDDM theme..."
 sudo cp -r ~/.dotfiles-cachyos/sddm/ /usr/share/sddm/themes/nordic-mountains/
 echo -e "[Theme]\nCurrent=nordic-mountains" | sudo tee /etc/sddm.conf
 
 echo "Copying configs..."
-cp ~/.dotfiles-cachyos/wallpapers/the-sphere.png ~/.config/hypr/wallpaper.png
+sudo cp ~/.dotfiles-cachyos/wallpapers/the-sphere.png ~/.config/hypr/wallpaper.png
 sudo cp ~/.dotfiles-cachyos/wallpapers/the-sphere.png /usr/share/sddm/themes/nordic-mountains/Backgrounds/wallpaper.png
 
 echo "Removing bloat..."
 sudo paru -R --noconfirm dolphin modemmanager zsh oh-my-zsh-git zsh-completions zsh-autosuggestions \
   cachyos-zsh-config zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k
+
+echo "Adding user to input group..."
+sudo usermod -aG input "$USER"
 
 echo "Creating docker group..."
 sudo systemctl enable --now docker
